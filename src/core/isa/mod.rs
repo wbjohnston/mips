d441c63fb3e377f0c7192850b24270e05782d5fe
Will Johnston <wbjohnston@gmail.com>
2017-10-26 15:47:11 -0400

    Fixed mod.rs not reflecting name of instruction

--- src/core/isa/mod.rs
+++ src/core/isa/mod.rs
@@ -4,7 +4,7 @@ mod opcode;
 pub use self::opcode::OpCode;
 
 mod instruction;
-pub use self::instruction::Instruction;
+pub use self::instruction::Instruction32;
 
 mod function_code;
 pub use self::function_code::FunctionCode;
